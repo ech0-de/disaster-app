6e06a073354ea4212bf65f7e9dad36e7d5390470
Dominik Meißner <dominik.meissner@uni-ulm.de>
2016-12-13 15:56:11 +0100

    add getting started section to readme

--- README.md
+++ README.md
@@ -3,9 +3,27 @@ disasterApp
 
 A prototypical triage management system based on commodity hardware and software components using a single-hop, ad-hoc network architecture with multi-master replication, a tablet-based device setup, and a mobile application for emergency services.
 
-
 Dependencies
 ------------
  * [CouchDB](https://couchdb.apache.org/)
  * [CouchApp](https://github.com/couchapp/couchapp)
  * [Node.js](https://nodejs.org/)
+
+Getting Started
+---------------
+
+### disasterApp
+ 1. Ensure that all dependencies are installed and available in your PATH
+ 1. Change to the `disasterApp` directory
+ 1. Run `npm install` to install all client dependencies with npm
+ 1. Run `gulp dist` to pack all client dependencies together
+ 1. Modify the `.couchapprc` according to your CouchDB configuration
+ 1. Run `npm run deploy` to push the application to your CouchDB instance
+
+Deploying disasterApp on a Raspberry Pi
+---------------------------------------
+![disasterApp on a Raspberry Pi](https://cloud.githubusercontent.com/assets/1503861/21144676/dfae9424-c14b-11e6-9a17-618e2e100b0b.jpg)
+ 1. Attach two WLAN USB dongles to the Raspberry Pi
+ 1. Configure one as an access point, through which a user device can be connected
+ 1. Configure the other one in IBSS mode
+ 1. Install all dependencies and follow the "Getting Started" instructions
--- disasterApp/gulpfile.js
+++ disasterApp/gulpfile.js
@@ -1,10 +1,23 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const minify = require('gulp-minify-css');
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', () => {
-  gulp.src([
+gulp.task('dist-css', () => {
+  return gulp.src([
+    'node_modules/font-awesome/css/font-awesome.css',
+    'node_modules/mobile-angular-ui/dist/css/mobile-angular-ui-hover.css',
+    'node_modules/mobile-angular-ui/dist/css/mobile-angular-ui-base.css',
+    'node_modules/mobile-angular-ui/dist/css/mobile-angular-ui-desktop.css',
+    'node_modules/leaflet/dist/leaflet.css'
+  ])
+    .pipe(cleanCSS())
+    .pipe(concat('dist.css'))
+    .pipe(gulp.dest('_attachments/css'));
+});
+
+gulp.task('dist-js', () => {
+  return gulp.src([
     'node_modules/angular/angular.js',
     'node_modules/angular-i18n/angular-locale_de-de.js',
     'node_modules/angular-route/angular-route.js',
@@ -23,21 +36,15 @@ gulp.task('default', () => {
     .pipe(uglify())
     .pipe(concat('dist.js'))
     .pipe(gulp.dest('_attachments/js'));
+});
 
-  gulp.src([
-    'node_modules/font-awesome/css/font-awesome.css',
-    'node_modules/mobile-angular-ui/dist/css/mobile-angular-ui-hover.css',
-    'node_modules/mobile-angular-ui/dist/css/mobile-angular-ui-base.css',
-    'node_modules/mobile-angular-ui/dist/css/mobile-angular-ui-desktop.css',
-    'node_modules/leaflet/dist/leaflet.css'
-  ])
-    .pipe(minify())
-    .pipe(concat('dist.css'))
-    .pipe(gulp.dest('_attachments/css'));
-
-  gulp.src([
+gulp.task('dist-fonts', () => {
+  return gulp.src([
     'node_modules/font-awesome/fonts/*'
   ])
     .pipe(gulp.dest('_attachments/fonts'));
 });
 
+gulp.task('dist', ['dist-css', 'dist-js', 'dist-fonts']);
+
+gulp.task('default', ['dist']);
